3a682e7dc2609294a4a7baa329b8c7a68b7cd033
happiness <happinesskhalan@gmail.com>
2025-05-11 19:16:28 +0300

    oop concept complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Initialize with None or default value
+        self.page_count = page_count  # This will call the setter method
+    
+    @property
+    def page_count(self):
+        """Getter method for page_count property"""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_value):
+        if isinstance(page_value, int):
+            self._page_count = page_value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    
+
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size, condition ="New"):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size_value):
+        if isinstance(size_value, int):
+            self._size = size_value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+
+        
+
+
+
+
+    
